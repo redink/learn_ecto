caec3fa9942a685cc0325c128589b459edfa37d5
taotao.lin <cnredink@gmail.com>
2019-07-07 22:19:57 +0800

    adjust pool_size

--- config/config.exs
+++ config/config.exs
@@ -2,7 +2,7 @@ use Mix.Config
 
 config :learn_ecto, LearnEcto.Database.Repo,
   log: :debug,
-  url: "ecto://postgres:postgres@localhost/learn_ecto_user"
+  url: "ecto://postgres:postgres@localhost/learn_ecto_user?pool_size=30"
 
 config :learn_ecto,
   ecto_repos: [LearnEcto.Database.Repo]
